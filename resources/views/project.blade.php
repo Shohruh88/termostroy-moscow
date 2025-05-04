d05e2447d4a1bdc437b45ea99f4a6ce5038d7865
Shohruh88 <shoh5288899@gmail.com>
2025-05-04 22:05:25 +0500

    bug fixed

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -8,7 +8,7 @@
     <div class="grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
         <!-- Chap rasm -->
         <div>
-            <img src="{{ asset('images/building.png') }}" alt="Building" class="w-full rounded-lg shadow-lg">
+            <img src="{{ asset('images/page_2.png') }}" alt="Building" class="w-full rounded-lg shadow-lg">
         </div>
 
         <!-- Project Steps List -->
--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -6,13 +6,13 @@
 <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
     <div class="grid grid-cols-2 sm:grid-cols-4 gap-2">
       <div class="space-y-2">
+        <img class="w-full h-auto object-cover" src="{{ asset('images/page_1.png') }}" alt="Gallery Image">
         <img class="w-full h-auto object-cover" src="{{ asset('images/page_7.png') }}" alt="Gallery Image">
-        <img class="w-full h-auto object-cover" src="{{ asset('images/page_9.png') }}" alt="Gallery Image">
         <img class="w-full h-auto object-cover" src="{{ asset('images/building.png') }}" alt="Gallery Image">
       </div>
       <div class="space-y-2">
         <img class="w-full h-auto object-cover" src="{{ asset('images/page_2.png') }}" alt="Gallery Image">
-        <img class="w-full h-auto object-cover" src="{{ asset('images/page_1.png') }}" alt="Gallery Image">
+        <img class="w-full h-auto object-cover" src="{{ asset('images/page_16.png') }}" alt="Gallery Image">
       </div>
       <div class="space-y-2">
         <img class="w-full h-auto object-cover" src="{{ asset('images/moscow-city.jpg') }}" alt="Gallery Image">
@@ -21,7 +21,7 @@
       </div>
       <div class="space-y-2">
         <img class="w-full h-auto object-cover" src="{{ asset('images/page_13.png') }}" alt="Gallery Image">
-        <img class="w-full h-auto object-cover" src="{{ asset('images/page_7.png') }}" alt="Gallery Image">
+        <img class="w-full h-auto object-cover" src="{{ asset('images/page_9.png') }}" alt="Gallery Image">
       </div>
     </div>
   </div>
