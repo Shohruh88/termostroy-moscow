ebc19fcde7033146077c0c5758e7555520f0b0c5
Shohruh88 <shoh5288899@gmail.com>
2025-05-08 22:48:56 +0500

    logo title add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,16 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Termostroy" name="description">
     <meta content="Проект" name="keywords">
+    <link rel="shortcut icon" href="{{ asset('images/logo-termo.png') }}">
 
     <title>@yield('title', 'Creative Agency')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
       [x-cloak] { display: none !important; }
   </style>
-  
+
 </head>
 <body class="text-gray-800 bg-white antialiased">
-  
+
     @include('layouts.header')
 
     @yield('content')
