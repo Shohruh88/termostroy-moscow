315bb3641c89c529874d8a8f2f9db27c878ae4df
Shohruh88 <shoh5288899@gmail.com>
2025-05-09 23:59:08 +0500

    languages add

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class LanguageController extends Controller
 {
+    public function switchLanguage(Request $request)
+    {
+        $locale = $request->input('locale');
+        // Store the locale in session
+        $request->session()->put('locale', $locale);
+        // Check if the request is an Inertia request
+        if ($request->inertia()) {
+            return redirect()->back();
+        }
 
+        return response()->json(['status' => 'success']);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,27 +5,30 @@ use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LanguageController;
 
+Route::post('language-switch', [LanguageController::class, 'switchLanguage'])->name('language.switch');
 
-// Route::get('/', function () {
-//     return view('home');
-// });
-
+Route::group([
+    'middleware' => 'locale',
+], function () {
 //service
-Route::get('/service', [ServiceController::class, 'index'])->name('service');
+    Route::get('/service', [ServiceController::class, 'index'])->name('service');
 
 //contact page
-Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+    Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 
-//HomeController 
-Route::get('/', [HomeController::class, 'index']);
+//HomeController
+    Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/about', [HomeController::class, 'about'])->name('about');
+    Route::get('/about', [HomeController::class, 'about'])->name('about');
 
-Route::get('/aboutUs', [HomeController::class, 'aboutUs'])->name('aboutUs');
+    Route::get('/aboutUs', [HomeController::class, 'aboutUs'])->name('aboutUs');
 
-Route::get('/project', [HomeController::class, 'project'])->name('project');
+    Route::get('/project', [HomeController::class, 'project'])->name('project');
 
-Route::get('/projects', function () {
-    return view('projects');
-})->name('projects');
+    Route::get('/projects', function () {
+        return view('projects');
+    })->name('projects');
+
+});
